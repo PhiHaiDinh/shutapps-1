371a97549318db88bdc11a098ca542bb91b310d8
ducle07 <duc.giang.le@web.de>
2017-04-23 19:00:55 +0200

    add facebook login, get and publish facebook profile id, get facebook friends

--- ControllingApps/app/src/main/java/pp_ss2017/controllingapps/FirebaseTestActivity.java
+++ ControllingApps/app/src/main/java/pp_ss2017/controllingapps/FirebaseTestActivity.java
@@ -1,33 +1,43 @@
 package pp_ss2017.controllingapps;
 
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.os.Build;
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequestAsyncTask;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class FirebaseTestActivity extends AppCompatActivity {
+public class FirebaseTestActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
@@ -35,13 +45,28 @@ public class FirebaseTestActivity extends AppCompatActivity {
     private TextView textView;
 
     private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
 
     private List<String> allContactNumbers = new ArrayList<>();
+
+    private FirebaseAuth firebaseAuth;
+
+    private GoogleApiClient mGoogleApiClient;
+    private Message mActiveMessage;
+    private MessageListener mMessageListener;
+
+    private String json;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_test);
+
+        firebaseAuth = firebaseAuth.getInstance();
+
+        if (AccessToken.getCurrentAccessToken() == null) {
+            goLoginScreen();
+        }
 
         textView = (TextView) this.findViewById(R.id.textView2);
 
@@ -58,7 +83,82 @@ public class FirebaseTestActivity extends AppCompatActivity {
         };
         myRef.child("message").addValueEventListener(valueEventListener);
 
-        checkPermission();
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
+                        // Application code
+                        Log.d("test", response.getJSONObject().toString());
+                        try {
+                            json = response.getJSONObject().getString("id");
+                        } catch(JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name");
+        request.setParameters(parameters);
+        request.executeAsync();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                buildClient();
+            }
+        }, 500);
+
+        mMessageListener = new MessageListener() {
+            String temp;
+
+            @Override
+            public void onFound(Message message) {
+                final String messageAsString = new String(message.getContent());
+                Log.d("nearby", "Found message: " + messageAsString);
+
+                GraphRequestAsyncTask friendRequest = new GraphRequest(
+                        AccessToken.getCurrentAccessToken(),
+                        "/me/friends",
+                        null,
+                        HttpMethod.GET,
+                        new GraphRequest.Callback() {
+                            @Override
+                            public void onCompleted(GraphResponse response) {
+                                try {
+                                    JSONArray friends = response.getJSONObject().getJSONArray("data");
+                                    for(int i=0; i<friends.length(); i++) {
+                                        Log.d("test", friends.get(i).toString());
+                                        JSONObject obj = friends.getJSONObject(i);
+                                        String name = obj.getString("name");
+                                        String id = obj.getString("id");
+                                        Log.d("test2", id);
+
+                                        if(messageAsString.equalsIgnoreCase(id)) {
+                                            temp = name;
+                                            Log.d("abgleich", name + " ist ein Freund und in der Nähe!");
+                                            myRef.child("groups").child("group"+i).setValue("Test");
+                                        }
+                                    }
+                                } catch(JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }
+                ).executeAsync();
+            }
+
+            @Override
+            public void onLost(Message message) {
+                String messageAsString = new String(message.getContent());
+                Log.d("nearby", "Lost sight of message: " + messageAsString);
+                Log.d("lost", temp + " ist nicht mehr in der Nähe!");
+            }
+        };
     }
 
     public void writeData(View view) {
@@ -71,6 +171,102 @@ public class FirebaseTestActivity extends AppCompatActivity {
         myRef.updateChildren(updateMap);
     }
 
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void logout(View view) {
+        firebaseAuth.signOut();
+        LoginManager.getInstance().logOut();
+        AccessToken.setCurrentAccessToken(null);
+        goLoginScreen();
+    }
+
+    public void buildClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Nearby.MESSAGES_API)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .build();
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.e("nearby", "GoogleApiClient disconnected with cause: " + cause);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (result.hasResolution()) {
+            try {
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch(IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.e("nearby", "GoogleApiClient connection failed");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+            if (resultCode == RESULT_OK) {
+                mGoogleApiClient.connect();
+            } else {
+                Log.e("nearby", "GoogleApiClient connection failed. Unable to resolve.");
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        publish(json);
+        subscribe();
+    }
+
+    @Override
+    public void onStop() {
+        unpublish();
+        unsubscribe();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    private void publish(String message) {
+        Log.i("nearby", "Publishing message: " + message);
+        mActiveMessage = new Message(message.getBytes());
+        Nearby.Messages.publish(mGoogleApiClient, mActiveMessage);
+    }
+
+    private void unpublish() {
+        Log.i("nearby", "Unpublishing.");
+        if (mActiveMessage != null) {
+            Nearby.Messages.unpublish(mGoogleApiClient, mActiveMessage);
+            mActiveMessage = null;
+        }
+    }
+
+    private void subscribe() {
+        Log.i("nearby", "Subscribing.");
+        Nearby.Messages.subscribe(mGoogleApiClient, mMessageListener);
+    }
+
+    private void unsubscribe() {
+        Log.i("nearby", "Unsubscribing.");
+        Nearby.Messages.unsubscribe(mGoogleApiClient, mMessageListener);
+    }
+
+
+
+
+    /*
     public void getContacts(View view) {
         Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
         while (phones.moveToNext())
@@ -82,6 +278,10 @@ public class FirebaseTestActivity extends AppCompatActivity {
         }
         Log.d("test", allContactNumbers.toString());
         phones.close();
+
+        TelephonyManager manager =(TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+        String mPhoneNumber = manager.getLine1Number();
+        System.out.println("test" + mPhoneNumber);
     }
 
     public void checkPermission() {
@@ -136,5 +336,5 @@ public class FirebaseTestActivity extends AppCompatActivity {
             // other 'case' lines to check for other
             // permissions this app might request
         }
-    }
+    }*/
 }
--- ControllingApps/app/src/main/java/pp_ss2017/controllingapps/MainActivity.java
+++ ControllingApps/app/src/main/java/pp_ss2017/controllingapps/MainActivity.java
@@ -1,6 +1,5 @@
 package pp_ss2017.controllingapps;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.support.design.widget.TabLayout;
@@ -15,24 +14,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.nearby.Nearby;
-import com.google.android.gms.nearby.messages.Message;
-import com.google.android.gms.nearby.messages.MessageListener;
-
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, AppInfoListener{
+public class MainActivity extends AppCompatActivity implements AppInfoListener{
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
-
-    private static final String TAG = "MainActivity";
-    private static final int REQUEST_RESOLVE_ERROR = 1001;
-
-    private GoogleApiClient mGoogleApiClient;
-    private Message mActiveMessage;
-    private MessageListener mMessageListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +34,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
-
-        buildGoogleApiClient();
     }
 
     @Override
@@ -59,108 +42,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         appData.setMessage(appName);
     }
 
-
-    private void buildGoogleApiClient() {
-        if (mGoogleApiClient != null) {
-            return;
-        }
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Nearby.MESSAGES_API)
-                .addConnectionCallbacks(this)
-                .enableAutoManage(this, this)
-                .build();
-
-        mMessageListener = new MessageListener() {
-            @Override
-            public void onFound(Message message) {
-                String messageAsString = new String(message.getContent());
-                Log.d(TAG, "Found message: " + messageAsString);
-            }
-
-            @Override
-            public void onLost(Message message) {
-                String messageAsString = new String(message.getContent());
-                Log.d(TAG, "Lost sight of message: " + messageAsString);
-            }
-        };
-    }
-
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        publish("Hello World");
-        subscribe();
-    }
-
-    @Override
-    public void onConnectionSuspended(int cause) {
-        Log.e(TAG, "GoogleApiClient disconnected with cause: " + cause);
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        if (result.hasResolution()) {
-            try {
-                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
-            }
-            catch (IntentSender.SendIntentException e) {
-            }
-        } else {
-            Log.e(TAG, "GoogleApiClient connection failed");
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_RESOLVE_ERROR) {
-            if (resultCode == RESULT_OK) {
-                mGoogleApiClient.connect();
-            } else {
-                Log.e(TAG, "GoogleApiClient connection failed. Unable to resolve.");
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
-    private void publish(String message) {
-        Log.i(TAG, "Publishing message: " + message);
-        mActiveMessage = new Message(message.getBytes());
-        Nearby.Messages.publish(mGoogleApiClient, mActiveMessage);
-    }
-
-    private void subscribe() {
-        Log.i(TAG, "Subscribing.");
-        Nearby.Messages.subscribe(mGoogleApiClient, mMessageListener);
-    }
-
-    private void unpublish() {
-        Log.i(TAG, "Unpublishing.");
-        if (mActiveMessage != null) {
-            Nearby.Messages.unpublish(mGoogleApiClient, mActiveMessage);
-            mActiveMessage = null;
-        }
-    }
-
-    private void unsubscribe() {
-        Log.i(TAG, "Unsubscribing.");
-        Nearby.Messages.unsubscribe(mGoogleApiClient, mMessageListener);
-    }
-
-    @Override
-    public void onStop() {
-        unpublish();
-        unsubscribe();
-        if (mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
-        }
-        super.onStop();
-    }
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
